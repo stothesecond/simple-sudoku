52ef5db5eaac2cbec133ad59d4b1fa5ba77871f4
Noshin Tasnim <ntasnim@hamilton.edu>
2022-12-02 16:29:30 -0500

    added boards

---
+++ ncurses/CMakeFiles/ncurses_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ncurses_test.dir/ncurses_test.cpp.o"
+  "ncurses_test"
+  "ncurses_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ncurses_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
