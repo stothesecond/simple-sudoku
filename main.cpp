38e6585ea4898a6df7d72eb0de524b8a3b91e726
Noshin Tasnim <ntasnim@hamilton.edu>
2022-12-02 17:30:25 -0500

    board

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
+#include <vector>
 #include <iostream>
-#include <string>
-
 int main(int argc, char const *argv[]) {
-  /* code */
+  std::vector<std::vector<int>> grid;
+
+  for (int i = 0; i < 9; ++i) {
+    grid.push_back({});
+    for (int j = 0; j < 9; ++j) {
+      grid[i].push_back(0);
+    }
+  }
+
+  // print_grid(grid);
+  for (int i = 0; i < 9; ++i) {
+    for (int j = 0; j < 9; ++j) {
+      std::cout << '[';
+
+      if (grid[i][j] == 0) {
+          std::cout << ' ';
+      } else {
+          std::cout << grid[i][j];
+      }
+
+      std::cout << ']';
+
+
+      if (j % 3 == 2) {
+          std::cout << "  ";
+      }
+    }
+    if (i % 3 == 2) {
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+
   return 0;
 }
